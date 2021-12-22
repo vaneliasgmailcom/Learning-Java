bf93db9b31f02bd456df14006e6bc1cdc042afb0
Vania <vaneliasviana@gmail.com>
2021-12-22 17:57:47 +1300

    my first commit

---
+++ Array.java
@@ -0,0 +1,66 @@
+/*public class Array{
+            
+    public static void main(String[] args) {
+        int[] age = { 12, 4, 5, 2,5};
+        System.out.println("Accessing Elements of Array:");
+        System.out.println("First Element:" + age[0]);
+        System.out.println("Second Element:" + age[1]);
+        System.out.println("Third Element:" + age[2]);
+        System.out.println("Fourth Element:" + age[3]);
+        System.out.println("Fifth Element:" + age[4]);
+
+    }
+}*/
+
+
+/*public class Array{
+            
+    public static void main(String[] args) {
+        String[] fruits = { "apple","banana","lime", "orange","persimon"};
+
+        System.out.println("Accessing Fruits of Array:");
+        System.out.println("First fruit:" + fruits[0]);
+        System.out.println("Second fruit:" + fruits[1]);
+        System.out.println("Third fruit:" + fruits[2]);
+        System.out.println("Fourth fruit:" + fruits[3]);
+        System.out.println("Fifth fruit:" + fruits[4]);
+
+    }
+}*/
+
+
+/*public class Array{
+            
+    public static void main(String[] args) {
+         String[] fruits = { "apple","banana","lime", "orange","persimon"};
+       
+        System.out.println("Using for loop:" );
+        for(int i = 0; i < fruits.length; i++){
+        System.out.println(fruits[i]);    
+        }
+    }
+}*/
+
+/*public class Array{
+            
+    public static void main(String[] args) {
+         int[] numbers = { 4, 5, 6, 7};
+       
+        System.out.println("Using for loop:" );
+        for(int number: numbers){
+        System.out.println(number);    
+        }
+    }
+}*/
+
+public class Array{
+            
+    public static void main(String[] args) {
+         String[] fruits = { "apple","banana","lime", "orange","persimon"};
+       
+        System.out.println("Using for loop:" );
+        for(string fruit: fruits){
+        System.out.println(fruits);    
+        }
+    }
+}
